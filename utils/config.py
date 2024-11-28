6b4f19c28f84477f84784c1ba47d871153e64250
Naruse <71993948+DevilProMT@users.noreply.github.com>
2024-11-28 16:20:54 +0800

    feat: Update to support version 7.9.0 OS

--- utils/config.py
+++ utils/config.py
@@ -20,6 +20,7 @@ class ConfigData:
     UseLocalCache: bool
     AESKeys: dict[str, str]
     EnableDispatchEncryption: bool
+    HotpatchConfig: dict[str, dict]
 
     def write_default_config():
         config = ConfigData(
@@ -31,9 +32,10 @@ class ConfigData:
             UseLocalCache=False,
             EnableDispatchEncryption=True,
             AESKeys={
-                "7.9.0_gf_pc": "36 31 65 37 64 33 65 66 33 32 30 63 31 35 66 66 61 64 37 61 66 32 31 34 61 64 65 64 32 34 33 38",
-                "7.8.0_os_pc": "64 34 32 33 30 30 31 62 32 36 38 34 62 33 62 30 61 33 30 38 66 37 65 35 63 30 61 38 66 33 65 32",
+                "7.9.0": "36 31 65 37 64 33 65 66 33 32 30 63 31 35 66 66 61 64 37 61 66 32 31 34 61 64 65 64 32 34 33 38",
+                "7.8.0": "64 34 32 33 30 30 31 62 32 36 38 34 62 33 62 30 61 33 30 38 66 37 65 35 63 30 61 38 66 33 65 32"
             },
+            HotpatchConfig=dict(),
         )
         with open("Config.json", "w") as f:
             f.write(json.dumps(asdict(config), indent=2))
@@ -49,6 +51,15 @@ class ConfigData:
                 return from_dict(ConfigData, json.load(f))
             except Exception:
                 return ConfigData.write_default_config()
+
+    def get_aes_key(self, version: str):
+        return self.AESKeys[version.split("_")[0]]
+
+    def get_hotpatch_manifest(self, version: str):
+        return self.HotpatchConfig.get(version, dict()).get("manifest", dict())
+
+    def get_hotpatch_ext(self, version: str):
+        return self.HotpatchConfig.get(version, dict()).get("ext", dict())
 
 
 Config = ConfigData.load()
